271fd8e9c1ae0217ae22ddc59811212bad7b94db
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2022-11-10 09:34:54 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,88 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../components/CSSReset";
+import Menu from "../components/Menu";
+import { StyledTimeline } from "../components/Timeline";
+
+function HomePage() {
+
+  return (
+    <>
+      <CSSReset />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+        }}
+      >
+        <Menu />
+        <Header />
+
+        <Timeline playlists={config.playlists} />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner"/> */}
+
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+function Timeline(props) {
+  const playlistNomes = Object.keys(props.playlists);
+  return (
+    <StyledTimeline>
+      {playlistNomes.map((playlistName) => {
+        const videos = props.playlists[playlistName];
+        return (
+          <section>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+}
